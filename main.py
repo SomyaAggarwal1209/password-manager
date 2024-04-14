03331ccd066ef895e1b915fb0bebffe7b80035e5
SomyaAggarwal1209 <117525704+SomyaAggarwal1209@users.noreply.github.com>
2024-04-14 12:32:27 +0530

    password generator

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
 
 PINK = "#FFB6B9"
 PEACH = "#FAE3D9"
-GREEN = "#BBDED6"
+GREEN = "#178329"
 TEAL = "#61C0BF"
 
 BLUE = "#2B2E4A"
@@ -13,6 +15,23 @@ FONT_NAME = "Courier"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.insert(0, password)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
@@ -20,15 +39,24 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        email_entry.delete(0, END)
-        password_entry.delete(0, END)
-        added_label = Label(text="Password Added Successfully!", fg=GREEN, font=(FONT_NAME, 8, "bold"), bg=PEACH)
-        added_label.grid(row=0, column=1)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops!", message="Please make sure you haven't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
+                                                              f"\nPassword: {password} \nIs it ok to save?")
+
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                email_entry.delete(0, END)
+                password_entry.delete(0, END)
+                added_label = Label(text="Password Added Successfully!", fg=GREEN, font=(FONT_NAME, 8, "bold"),
+                                    bg=PEACH)
+                added_label.grid(row=0, column=1)
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=60, pady=60, bg=PEACH)
@@ -36,31 +64,31 @@ window.config(padx=60, pady=60, bg=PEACH)
 canvas = Canvas(width=200, height=200, bg=PEACH, highlightthickness=0)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
-canvas.grid(row=0, column=1)
+canvas.grid(row=1, column=1)
 
 #Labels
 website_label = Label(text="Website:", fg=BLUE, font=(FONT_NAME, 12), bg=PEACH)
-website_label.grid(row=1, column=0)
+website_label.grid(row=2, column=0)
 email_label = Label(text="Email/Username:", fg=BLUE, font=(FONT_NAME, 12), bg=PEACH)
-email_label.grid(row=2, column=0)
+email_label.grid(row=3, column=0)
 password_label = Label(text="Password:", fg=BLUE, font=(FONT_NAME, 12), bg=PEACH)
-password_label.grid(row=3, column=0)
+password_label.grid(row=4, column=0)
 
 #Entries
 website_entry = Entry(width=52)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=2, column=1, columnspan=2)
 website_entry.focus()
 email_entry = Entry(width=52)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.grid(row=3, column=1, columnspan=2)
 email_entry.insert(0, "dummy_email@gmail.com")
 password_entry = Entry(width=34)
-password_entry.grid(row=3, column=1)
+password_entry.grid(row=4, column=1)
 
 #Buttons
-generate_password_button = Button(text="Generate Password", width=14)
-generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=44)
-add_button.grid(row=4, column=1, columnspan=2)
+generate_password_button = Button(text="Generate Password", width=14, command=generate_password)
+generate_password_button.grid(row=4, column=2)
+add_button = Button(text="Add", width=44, command=save)
+add_button.grid(row=5, column=1, columnspan=2)
 
 window.mainloop()
 
