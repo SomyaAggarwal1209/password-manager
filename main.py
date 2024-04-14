75514e0f6d84b66b9eb78034df419d737a000d0a
SomyaAggarwal1209 <117525704+SomyaAggarwal1209@users.noreply.github.com>
2024-04-14 12:35:09 +0530

    pyperclip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
+import pyperclip
 
 PINK = "#FFB6B9"
 PEACH = "#FAE3D9"
@@ -31,6 +32,7 @@ def generate_password():
     password = "".join(password_list)
 
     password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
